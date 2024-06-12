e751c2d3bd0d5aa7e5d9edbe8314b2121877c3aa
novventa <novventa@kakao.com>
2024-06-12 21:37:45 +0900

    :bug: Fix KakaoApiClient import list(HttpHeaders)

--- src/main/java/nova/mysub/global/auth/oauth2/KakaoApiClient.java
+++ src/main/java/nova/mysub/global/auth/oauth2/KakaoApiClient.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.net.http.HttpHeaders;
+import org.springframework.http.HttpHeaders;
 
 @FeignClient(name = "kakaoApiClient", url = "https://kapi.kakao.com")
-public class KakaoApiClient {
+public interface KakaoApiClient {
 
     @GetMapping(value = "/v2/user/me")
     KakaoUserResponse getUserInformation(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken);
