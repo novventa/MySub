20435540bacde746faf8b2757bfd7c58b5688a46
novventa <novventa@kakao.com>
2024-12-10 18:18:47 +0900

    :tada: Init Project

---
+++ src/test/java/nova/mysub/MySubApplicationTests.java
@@ -0,0 +1,13 @@
+package nova.mysub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
